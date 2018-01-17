89035193f6f2e47ea0bf483f9a7ea5fd4429ff30
John Polling <john@theled.co.uk>
2018-01-17 13:37:19 +0000

    chore: update minor version 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ecto.UKPostcode.Mixfile do
 
   def project do
     [app: :ecto_uk_postcode,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
